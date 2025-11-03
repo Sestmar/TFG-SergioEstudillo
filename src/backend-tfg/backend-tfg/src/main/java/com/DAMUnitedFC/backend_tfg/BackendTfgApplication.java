d330898a31c28650fec70c989ea0e510eb40bb24
Sestmar <sergio.estudilloo7@gmail.com>
2025-11-03 13:04:10 +0100

    Inicialización del backend Spring Boot y estructura en src/backend-tfg

---
+++ src/backend-tfg/backend-tfg/src/main/java/com/DAMUnitedFC/backend_tfg/BackendTfgApplication.java
@@ -0,0 +1,13 @@
+package com.DAMUnitedFC.backend_tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTfgApplication.class, args);
+	}
+
+}
---
+++ src/backend-tfg/backend-tfg/src/test/java/com/DAMUnitedFC/backend_tfg/BackendTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAMUnitedFC.backend_tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
